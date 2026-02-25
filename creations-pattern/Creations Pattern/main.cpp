f8ebebe3a3542e0ff1ea0f23bbd6304749b9a340
mimicryknife-a11y <mimicryknife@gmail.com>
2026-02-25 19:26:01 +0500

    Creations Pattern update
    + getHealth() method
    + Knight class

--- creations-pattern/Creations Pattern/main.cpp
+++ creations-pattern/Creations Pattern/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 class Character {
 public:
 	virtual string getType() = 0;
+	virtual int getHealth() = 0;
 	virtual void attack() = 0;
 };
 
@@ -14,18 +15,28 @@ class Mage : public Character {
 public:
 	string getType() override { return "Mage"; }
 	void attack() override { cout << "Маг уничтожает хромасому врага!" << endl; }
+	int getHealth() override { return 100; }
 };
 
 class Warrior : public Character {
 public:
 	string getType() override { return "Warrior"; }
 	void attack() override { cout << "Воин махает мечом!" << endl; }
+	int getHealth() override { return 90; }
+};
+
+class Knight : public Character {
+public:
+	string getType() override { return "Knight"; }
+	void attack() override { cout << "Рыцарь атакует копьём!" << endl; }
+	int getHealth() override { return 80; }
 };
 
 class Archer : public Character {
 public:
 	string getType() override { return "Archer"; }
 	void attack() override { cout << "Лучник выпускает дождь стрел!" << endl; }
+	int getHealth() override { return 70; }
 };
 
 class CharacterFactory {
@@ -33,7 +44,7 @@ public:
 	virtual Character* createCharacter() = 0;
 	void spawnAndAttack() {
 		Character* character = createCharacter();
-		cout << "Создан персонаж с типом " << character->getType() << endl;
+		cout << "Создан персонаж с типом " << character->getType() << " (" << character->getHealth() << " hp)" << endl;
 		character->attack();
 		delete character;
 	}
@@ -47,6 +58,11 @@ public:
 class WarriorFactory : public CharacterFactory {
 public:
 	Character* createCharacter() override { return new Warrior(); }
+};
+
+class KnightFactory : public CharacterFactory {
+public:
+	Character* createCharacter() override { return new Knight(); }
 };
 
 class ArcherFactory : public CharacterFactory {
@@ -68,6 +84,11 @@ int main() {
 		clownFactory->spawnAndAttack();
 
 	clownFactory = new ArcherFactory();
+
+	for (short int i = 0; i < 10; i++)
+		clownFactory->spawnAndAttack();
+
+	clownFactory = new KnightFactory();
 
 	for (short int i = 0; i < 10; i++)
 		clownFactory->spawnAndAttack();
